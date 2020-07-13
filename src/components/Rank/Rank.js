20052520113a17379fff085e10ab0e013bb9f1a2
Rayson Kong <kong.rayson@gmail.com>
2020-07-13 19:03:50 +0800

    added particles background

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rank = () => {
+    return(
+        <div className="tc">
+           <div>
+             <h2>{'Hello Rayson, your rank is: '}</h2>
+           </div>
+
+           <div>
+             <h2>{'Number 5'}</h2>
+           </div>
+        </div>
+    );
+}
+
+export default Rank;
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,13 +4,18 @@ import 'tachyons';
 import Navigation from '../components/Navigation/Navigation';
 import Logo from '../components/Logo/Logo';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
+import Rank from '../components/Rank/Rank';
+import Particles from 'react-particles-js';
+
 
 class App extends React.Component {
   render() {
     return(
       <div> 
+        <Particles className="particles"/>
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
       </div>
     )
